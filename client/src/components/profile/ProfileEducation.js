90bd641043bea2ba2d93bb50ab6555ac5592298d
Yonghao Shu <123shuyonghao@gmail.com>
2021-03-12 00:06:33 -0800

    Initial version

---
+++ client/src/components/profile/ProfileEducation.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Moment from 'react-moment'
+
+const ProfileEducation = ({education: {
+    school,
+    degree,
+    fieldOfStudy,
+    current,
+    to,
+    from,
+    description
+}}) => {
+    return (
+        <div>
+            <div>
+                <h3 className="text-dark">{school}</h3>
+            </div>
+            <p>
+                <Moment format='YYYY/MM/DD'>{from}</Moment> - {!to ? ' Now' : <Moment format='YYYY/MM/DD'>{from}</Moment>}
+            </p>
+            <p>
+                <strong>Position: </strong> {degree}
+            </p>
+            <p>
+                <strong>Field Of Study: </strong> {fieldOfStudy}
+            </p>
+            <p>
+                <strong>Description: </strong> {description}
+            </p>
+        </div>
+    )
+}
+
+ProfileEducation.propTypes = {
+    education: PropTypes.object.isRequired,
+}
+
+export default ProfileEducation
